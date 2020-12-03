1ef01255e3074ae6c751b2aa067c902ea6a8f4bb
kostya <caxnyk2002@gmail.com>
2020-12-03 09:00:05 +0300

    .

--- keys/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MONGODB_URI: 'mongodb+srv://kostya:YfZhbwgPm6NcVNcs@cluster0.5ouzx.mongodb.net/shop',
-  SESSION_SECRET: 'secret string',
-  GRID_API_KEY: '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',
-  EMAIL_FROM: 'caxnyk2002@gmail.com',
-  BASE_URL: 'http://localhost:3000',
-};
